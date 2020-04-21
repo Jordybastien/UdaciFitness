9930dc1f383bd0b9cff0aa7ef3b1be01ff3d0403
Jordy Bastien Rugumbira <rugumbirajordybastien@gmail.com>
2020-04-21 22:59:40 +0200

    More Styling

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AddEntry from './components/AddEntry';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import History from './components/History';
 
 class App extends Component {
   state = {};
@@ -11,7 +12,8 @@ class App extends Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <View style={{ height: 20 }} />
+          <History />
         </View>
       </Provider>
     );
