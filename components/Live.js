e84656fac76626ce8ce76a83ece3b44ef0e0ac42
Jordy Bastien Rugumbira <rugumbirajordybastien@gmail.com>
2020-04-24 15:24:56 +0200

    Animations

--- components/Live.js
+++ components/Live.js
@@ -5,6 +5,7 @@ import {
   ActivityIndicator,
   TouchableOpacity,
   StyleSheet,
+  Animated,
 } from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import { purple, white } from '../utils/colors';
@@ -17,6 +18,7 @@ export default class Live extends Component {
     coords: null,
     status: null,
     direction: '',
+    bounceValue: new Animated.Value(1),
   };
   componentDidMount() {
     Permissions.getAsync(Permissions.LOCATION)
@@ -56,6 +58,13 @@ export default class Live extends Component {
       },
       ({ coords }) => {
         const newDirection = calculateDirection(coords.heading);
+        const { direction, bounceValue } = this.state;
+        if (newDirection !== direction) {
+          Animated.sequence([
+            Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4 }),
+          ]).start();
+        }
         this.setState(() => ({
           coords,
           status: 'granted',
@@ -66,7 +75,7 @@ export default class Live extends Component {
   };
 
   render() {
-    const { status, coords, direction } = this.state;
+    const { status, coords, direction, bounceValue } = this.state;
 
     if (status === null) {
       return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -100,16 +109,24 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>{direction}</Text>
+          <Animated.Text
+            style={[styles.direction, { transform: [{ scale: bounceValue }] }]}
+          >
+            {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Altitude</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{Math.round(coords.altitude * 3.2808)} Feet</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {Math.round(coords.altitude * 3.2808)} Feet
+            </Text>
           </View>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Speed</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{(coords.speed * 2.2369).toFixed(1)} MPH</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {(coords.speed * 2.2369).toFixed(1)} MPH
+            </Text>
           </View>
         </View>
       </View>
